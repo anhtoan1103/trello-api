89cb1009d0ec6215919f7286f7ce78a9f02aab15
Tra Anh Toan <toantra732@gmail.com>
2023-10-15 01:58:15 +0700

    create simple router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,1 +1,13 @@
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
 
+const Router = express.Router()
+
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'api ready to use' })
+})
+
+Router.use('/boards', boardRoutes)
+
+export const APIs_V1 = Router
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import express from 'express'
 import AsyncExitHook from 'async-exit-hook'
 import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from './config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
   const app = express()
@@ -13,6 +14,8 @@ const START_SERVER = () => {
     console.log(await GET_DB().listCollections().toArray())
     res.end('<h1>Hello World!</h1><hr>')
   })
+
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
