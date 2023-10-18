ce4e37f8d62ebacc83bcd8b25714fa9ac25251e1
Tra Anh Toan <toantra732@gmail.com>
2023-10-18 23:32:14 +0700

    create model

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,10 +1,29 @@
-import mongoose from 'mongoose'
-const BoardSchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Types.ObjectId,
-    auto: true
-  },
-  title: String
+// import mongoose from 'mongoose'
+// const BoardSchema = new mongoose.Schema({
+//   _id: {
+//     type: mongoose.Types.ObjectId,
+//     auto: true
+//   },
+//   title: String
+// })
+
+// module.exports = BoardSchema
+
+import Joi from 'joi'
+
+// Define collection
+const BOARD_COLLECTION_NAME = 'boards'
+const BOARD_COLLECTION_SCHEMA = Joi.object({
+  title: Joi.string().required().min(3).max(50).trim().strict(),
+  slug: Joi.string().required().min(3).trim().strict(),
+  description: Joi.string().required().min(3).max(256).trim().strict(),
+  columnOrderIds: Joi.array().items(Joi.string()).default(),
+  createdAt: Joi.date().timestamp('javascript').default(Date.now()),
+  updatedAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
 })
 
-module.exports = BoardSchema
+export const boardModel = {
+  BOARD_COLLECTION_NAME,
+  BOARD_COLLECTION_SCHEMA
+}
---
+++ src/models/columnModel.js
@@ -0,0 +1,18 @@
+import Joi from 'joi'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+
+const COLUMN_COLLECTION_NAME = 'boards'
+const COLUMN_COLLECTION_SCHEMA = Joi.object({
+  boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+  title: Joi.string().required().min(3).max(50).trim().strict(),
+  cardOrderIds: Joi.array().items(Joi.string()).default([]),
+
+  createdAt: Joi.date().timestamp('javascript').default(Date.now()),
+  updatedAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+export const columnModel = {
+  COLUMN_COLLECTION_NAME,
+  COLUMN_COLLECTION_SCHEMA
+}
