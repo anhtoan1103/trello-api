76d711445984615d3661615e1b98f18c6d4f4dc2
Tra Anh Toan <toantra732@gmail.com>
2023-10-19 01:43:50 +0700

    create an api to get details of board

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -10,4 +10,8 @@ Router.route('/').get((req, res) => {
 
 }).post(boardValidation.createNew, boardController.createNew)
 
+Router.route('/:id')
+  .get(boardController.getDetails)
+  .put()
+
 export const boardRoutes = Router
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -2,6 +2,7 @@
 import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (reqBody) => {
   try {
@@ -18,6 +19,19 @@ const createNew = async (reqBody) => {
   }
 }
 
+const getDetails = async (boardId) => {
+  try {
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    }
+    return board
+  } catch (error) {
+    throw error
+  }
+}
+
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
