2d6a88617601f3443a97d3814ef9ae1765a809d0
Tra Anh Toan <toantra732@gmail.com>
2023-10-21 22:11:28 +0700

    config cors

---
+++ src/config/cors.js
@@ -0,0 +1,20 @@
+import { WHITELIST_DOMAINS } from '~/utils/constants'
+import { env } from '~/config/environment'
+import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
+
+export const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin && env.BUILD_MODE === 'dev') {
+      return callback(null, true)
+    }
+
+    if (WHITELIST_DOMAINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS policy.`))
+  },
+  optionSuccessStatus: 200,
+  credential: true
+}
